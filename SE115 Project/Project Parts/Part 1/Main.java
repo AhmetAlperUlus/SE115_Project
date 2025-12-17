17905386276fba0261263d0cbe965b15113a5699
AhmetAlperUlus <ahmetalperulus@gmail.com>
2025-12-18 00:38:36 +0300

    SE115 Project - Part 1

---
+++ SE115 Project/Project Parts/Part 1/Main.java
@@ -0,0 +1,136 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner sc = null;
+
+    static final int MONTHS = 12;
+    static final int DAYS = 28;
+    static final int COMMS = 5;
+    static String[] commodities = {"Gold", "Oil", "Silver", "Wheat", "Copper"};
+    static String[] months = {"January","February","March","April","May","June", "July","August","September","October","November","December"};
+
+    public static int[][][] data = new int[MONTHS][DAYS][COMMS];
+
+    public static int getCommoditiesIndex(String name) {
+        for (int i = 0 ; i < COMMS ; i++) {
+            if (commodities[i].equals(name)) return i;
+        }
+        return -9999;
+    }
+
+    public static void loadData() {
+        for (int i = 0 ; i < MONTHS ; i++) {
+            String fileName =  "./Data_Files/" + months[i] + ".txt";
+            File file = new File(fileName);
+
+            if (!(file.exists())) continue;
+
+            try {
+                Scanner sc = new Scanner(file);
+                if (sc.hasNextLine()) sc.nextLine();
+
+                while (sc.hasNextLine()) {
+                    String line = sc.nextLine();
+
+                    String[] dataParts = line.split(",");
+
+                    if (dataParts.length == 3) {
+                        int day = Integer.parseInt(dataParts[0].trim());
+                        String commName = dataParts[1].trim();
+                        int profit = Integer.parseInt(dataParts[2].trim());
+
+                        int cIndex = getCommoditiesIndex(commName);
+
+                        if (day >= 1 && day <= DAYS && cIndex != -1) {
+                            data[i][day - 1][cIndex] = profit;
+                        }
+                    }
+                }
+            }
+
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            finally {
+                sc.close();
+            }
+
+        }
+    }
+
+    // ======== 10 REQUIRED METHODS (Students fill these) ======== (2/10 done)
+
+    public static String mostProfitableCommodityInMonth(int month) {
+        if (month < 0 || month >= MONTHS) return "Invalid Month";
+
+        long maxProfit = Long.MIN_VALUE;
+        int maxIndex = -1;
+
+        for (int i = 0 ; i < COMMS ; i++) {
+            long total = 0;
+
+            for (int j = 0 ; j < DAYS ; j++) {
+                total += data[month][j][i];
+            }
+
+            if (total > maxProfit) {
+                maxProfit = total;
+                maxIndex = i;
+            }
+        }
+
+        if (maxIndex == -1) return "No Data";
+
+        return commodities[maxIndex] + " ===>" + maxProfit;
+    }
+
+    public static int totalProfitOnDay(int month, int day) {
+        if (month < 0 || month > MONTHS || day < 1 || day > DAYS) return -6666;
+
+        int total = 0;
+        for (int i = 0 ; i < COMMS ; i++) {
+            total += data[month][day - 1][i];
+        }
+
+        return total;
+    }
+
+    public static int commodityProfitInRange(String commodity, int from, int to) {
+        return 1234;
+    }
+
+    public static int bestDayOfMonth(int month) {
+        return 1234;
+    }
+
+    public static String bestMonthForCommodity(String comm) {
+        return "DUMMY";
+    }
+
+    public static int consecutiveLossDays(String comm) {
+        return 1234;
+    }
+
+    public static int daysAboveThreshold(String comm, int threshold) {
+        return 1234;
+    }
+
+    public static int biggestDailySwing(int month) {
+        return 1234;
+    }
+
+    public static String compareTwoCommodities(String c1, String c2) {
+        return "DUMMY is better by 1234";
+    }
+
+    public static String bestWeekOfMonth(int month) {
+        return "DUMMY";
+    }
+
+    public static void main(String[] args) {
+        loadData();
+        System.out.println("Data loaded – ready for queries");
+    }
+}
